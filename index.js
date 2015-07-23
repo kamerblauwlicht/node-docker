3b1a245765f519fb5e962d6f84b964bf7287f4ba
Khash Sajadi <khash@cloud66.com>
2015-07-23 18:30:19 +0100

    some styling for the show

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express    = require('express');
 var mysql      = require('mysql');
+var os = require("os");
 var connection = mysql.createConnection({
   host     : process.env.MYSQL_ADDRESS,
   user     : process.env.MYSQL_USERNAME,
@@ -27,7 +28,7 @@ connection.query("CREATE TABLE IF NOT EXISTS tests ( counter INT(6) UNSIGNED );"
 app.get("/",function(req,res){
   connection.query('SELECT counter from tests LIMIT 1', function(err, rows, fields) {
     if (!err) {
-      res.send('So far I have ' + rows[0].counter);
+      res.send('<span style="font-family: Arial; font-size: 30px;">I\'m running on <b>' + os.hostname() + '</b> and so far I\'v had <b>' + rows[0].counter + '</b> hits</span>');
       connection.query('UPDATE tests SET counter = counter + 1;');
     } else {
       console.log('Error while performing Query.');
